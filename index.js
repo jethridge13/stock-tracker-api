d8d3f2ff1786fb6577d9fbf6c18366c116574add
Josh Ethridge <joshua.ethridge13@gmail.com>
2019-05-30 21:04:38 -0400

    Server no longer crashes when not provided with input symbol

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const express = require('express');
 const request = require('request');
 const cors = require('cors');
+const _ = require('lodash');
 
 // Network variables
 const app = express();
@@ -15,25 +16,23 @@ const URL = 'https://www.alphavantage.co/';
 app.use(cors());
 
 app.get('/get', function (req, res) {
-	const symbols = req.query.symbols.split(',');
-	const data = [];
-	symbols.forEach((ticker, index, array) => {
-		request(URL + `query?function=TIME_SERIES_DAILY&symbol=${ticker}&apikey=${KEY}`, {json: true}, (err, r, body) => {
-			console.log(`Requesting ${ticker}`);
-      // TODO Make this more robust
-      const symbol = body['Meta Data']['2. Symbol'];
-      const dates = body['Time Series (Daily)'];
+  const symbols = req.query.symbols.split(',');
+  const data = [];
+  symbols.forEach((ticker, index, array) => {
+    request(URL + `query?function=TIME_SERIES_DAILY&symbol=${ticker}&apikey=${KEY}`, {json: true}, (err, r, body) => {
+      console.log(`Requesting ${ticker}`);
+      const symbol = _.get(body, ['Meta Data', '2. Symbol'], '');
+      const dates = _.get(body, 'Time Series (Daily)', []);
       const response = {
         symbol,
         dates,
       }
-      console.log(response);
-			data.push(response);
-			if (index === array.length - 1) {
-				res.send(data);
-			}
-		});
-	});
+      data.push(response);
+      if (index === array.length - 1) {
+        res.send(data);
+      }
+    });
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
